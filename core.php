00f0932b695033e6c417ea0204bcec0aec8a883d
Ancil K Anto <contactancil@gmail.com>
2018-01-11 18:40:12 +0530

    Core file created

---
+++ core.php
@@ -0,0 +1,12 @@
+<?php
+/**
+Plugin Name: Q Core
+Plugin URI: http://quadnotion.com/
+Description: Plugin developed by Quadnotion to for Truck Theme.
+Author: Quadnotion
+Author URI: http://quadnotion.com/
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: trucking
+Version: 1.0
+ */
