48d012100d42110bb9afff5b95a8e1ea905efc3d
Ancil K Anto <contactancil@gmail.com>
2018-02-08 17:19:24 +0530

    new meta option for thumbnail image added

--- truck_listing/truck_listing.php
+++ truck_listing/truck_listing.php
@@ -61,7 +61,7 @@ function truck_type_tax_metabox() {
 	$cmb_truck_type->add_field( array(
 	    'name'    => esc_html__( 'Icon Image', 'qcore' ),
 		'desc'    => esc_html__( 'Select image from media library/Upload new image. Use image of width 72px for better appearance in larger displays.', 'qcore' ),
-		'id'      => $prefix . 'portfolio_thumb',
+		'id'      => $prefix . 'thumb',
 		'type'    => 'file',
 		'allow'   => array( 'attachment' ),
 		"options" => array(
@@ -73,6 +73,43 @@ function truck_type_tax_metabox() {
 }
 
 
+
+
+add_action( 'cmb2_admin_init', 'truck_listings_metabox' );
+/**
+ * Hook in and add a demo metabox. Can only happen on the 'cmb2_init' hook.
+ */
+function truck_listings_metabox() {
+
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = '_truck_list_';
+	/**
+	 * Sample metabox to demonstrate each field type included
+	 */
+	$cmb_truck_list = new_cmb2_box( array(
+		'id'            => $prefix . 'metabox',
+		'title'         => esc_html__( 'Additional Options', 'qcore' ),
+		'object_types'  => array( 'listings' ),
+		'context'       => 'normal',
+		'priority'      => 'high',
+		'show_names'    => true, // Show field names on the left
+		'cmb_styles'    => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // true to keep the metabox closed by default
+	) );
+
+	$cmb_truck_list->add_field( array(
+	    'name'    => esc_html__( 'Thumbnail Image', 'qcore' ),
+		'desc'    => esc_html__( 'Select image from media library/Upload new image. Use image of width 72px for better appearance in larger displays.', 'qcore' ),
+		'id'      => $prefix . 'thumb',
+		'type'    => 'file',
+		'allow'   => array( 'attachment' ),
+		"options" => array(
+				        "url" => false
+				     )
+	    
+	) );
+	
+}
 
 
 
